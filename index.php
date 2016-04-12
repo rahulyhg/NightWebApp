64bc2188136ec6615a18598eaa5a3d80d68587db
TusharSachde <tushar@wohlig.com>
2016-04-12 19:00:38 +0530

    Fixes

--- index.php
+++ index.php
@@ -4,15 +4,26 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<meta name="description" content="Build Android &amp; iOS Application in 3 steps.">
+	<meta name="author" content="Blazen">
+	<meta name="keywords" content="Blazen">
+
+	<meta property="og:site_name" content="Blazen">
+	<meta property="og:title" content="Blazen" />
+	<meta property="og:description" content="Build Android &amp; iOS Application in 3 steps." />
+	<meta property="og:type" content="website" />
+
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@blazendev">
+	<meta name="twitter:title" content="Blazen">
+	<meta name="twitter:description" content="Build Android &amp; iOS Application in 3 steps.">
+	<meta name="twitter:creator" content="@blazendev">
 
 	<title>Blazen | Coming Soon</title>
 
-
 </head>
 <body style="margin:0;">
-<img src="image.jpg" style="width:100%;height:auto;"/>
+<img src="image.jpg" style="width:100%;height:auto;max-width:1500px;"/>
 </body>
 
 </html>
