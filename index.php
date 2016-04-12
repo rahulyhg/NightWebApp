6997632b7266f9e3cfdaa9d86e308f043ca4e5a9
Chintan <chintan@wohlig.com>
2016-04-12 18:43:42 +0530

    ccc

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Blazen | Coming Soon</title>
+
+
+</head>
+<body style="margin:0;">
+<img src="image.jpg" style="width:100%;height:auto;"/>
+</body>
+
+</html>
